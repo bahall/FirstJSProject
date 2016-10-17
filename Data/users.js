ce3ed76645568e804aef50a2fec7357f771183fd
Brent Hall <hallba@umail.iu.edu>
2016-10-17 15:37:29 -0400

    working on storing user data

--- Data/users.js
+++ Data/users.js
@@ -1,21 +1,18 @@
 var fs = require('fs');
 var data = require('./dataFunctions.js');
-var usersFile = require('./users.txt');
+var usersFile = require('./users.json');
 
 var users = new Array(4);
 for(var i = 0; i < 4; i++){
     users[i] = [];
 }
 
-var readToArray = function(event) {
-        var input = event.target;
-
-        var reader = new FileReader();
-        reader.onload = function(){
-          var text = reader.result;
-          var node = document.getElementById('output');
-          node.innerText = text;
-          console.log(reader.result.substring(0, 200));
-        };
-        reader.readAsText(input.files[0]);
-      };
+var names = usersFile.Names;
+var dates = usersFile.FollowDate;
+var titles = usersFile.Title;
+var votes = usersFile.Vote;
+users[0] = names.split(" ");
+users[1] = dates.split(" ");
+users[2] = titles.split(" ");
+users[3] = votes.split(" ");
+console.log(users[0][0]+" first visited this asshole on "+users[1][0]);
